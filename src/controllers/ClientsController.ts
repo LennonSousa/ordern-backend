fea4a9956db6d4890ced048852510a74cb7f4c52
lennonsousa <lennonsousa@outlook.com>
2020-12-10 14:12:50 -0300

    client boolean castging

--- src/controllers/ClientsController.ts
+++ src/controllers/ClientsController.ts
@@ -30,7 +30,7 @@ export default {
     },
 
     async create(request: Request, response: Response) {
-        const {
+        let {
             name,
             cpf,
             birth,
@@ -42,6 +42,9 @@ export default {
             address,
             payments
         } = request.body;
+
+        active = active === 'true' ? true : false;
+        paused = paused === 'true' ? true : false;
 
         const clientsRepository = getRepository(ClientsModel);
 
@@ -106,7 +109,7 @@ export default {
     async update(request: Request, response: Response) {
         const { id } = request.params;
 
-        const {
+        let {
             name,
             cpf,
             birth,
@@ -117,6 +120,9 @@ export default {
             address,
             payments
         } = request.body;
+
+        active = active === 'true' ? true : false;
+        paused = paused === 'true' ? true : false;
 
         const clientsRepository = getRepository(ClientsModel);
 
