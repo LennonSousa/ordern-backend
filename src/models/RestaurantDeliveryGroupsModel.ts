0dfd7d71337cd29e9e4d8e37ad820111d0a6a6de
lennonsousa <lennonsousa@outlook.com>
2021-01-01 14:09:42 -0300

    Restaurant delivery groups model name fixed

--- src/models/RestaurantDeliveryGroupsModel.ts
+++ src/models/RestaurantDeliveryGroupsModel.ts
@@ -1,7 +1,7 @@
 import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity('restaurant_delivery_groups')
-export default class ProductValuesModel {
+export default class RestaurantDeliveryGroupsModel {
     @PrimaryGeneratedColumn()
     id: number;
 
