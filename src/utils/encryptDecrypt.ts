fc75b94125423906e7ac21dd424c2521ce4ff3f1
lennonsousa <lennonsousa@outlook.com>
2021-01-06 12:25:04 -0300

    Encrypt and decrypt function to card number

--- src/utils/encryptDecrypt.ts
+++ src/utils/encryptDecrypt.ts
@@ -1,28 +1,42 @@
 require('dotenv/config');
 import crypto from 'crypto';
 
-const algorithm = 'aes-256-ctr';
-const ivKey = crypto.randomBytes(16);
+const algorithm = 'sha256';
 
 export function encrypt(text: string) {
-    if (process.env.JWT_SECRET) {
-        const cipher = crypto.createCipheriv(algorithm, process.env.JWT_SECRET, ivKey);
+    if (process.env.PUBLIC_KEY) {
+        const encryptedData = crypto.publicEncrypt(
+            {
+                key: process.env.PUBLIC_KEY.replace(/\\n/gm, '\n'),
+                padding: crypto.constants.RSA_PKCS1_OAEP_PADDING,
+                oaepHash: algorithm,
+            },
+            // We convert the data string to a buffer using `Buffer.from`
+            Buffer.from(text)
+        );
 
-        const encrypted = Buffer.concat([cipher.update(text), cipher.final()]);
-
-        return encrypted.toString('hex');
+        return encryptedData.toString('base64');
     }
 
     return text;
 };
 
 export function decrypt(hash: string) {
-    if (process.env.JWT_SECRET) {
-        const decipher = crypto.createDecipheriv(algorithm, process.env.JWT_SECRET, ivKey);
+    if (process.env.PRIVATE_KEY) {
+        const decryptedData = crypto.privateDecrypt(
+            {
+                key: process.env.PRIVATE_KEY.replace(/\\n/gm, '\n'),
+                // In order to decrypt the data, we need to specify the
+                // same hashing function and padding scheme that we used to
+                // encrypt the data in the previous step
+                padding: crypto.constants.RSA_PKCS1_OAEP_PADDING,
+                oaepHash: algorithm,
+            },
+            Buffer.from(hash, 'base64')
+        );
 
-        const decrpyted = Buffer.concat([decipher.update(Buffer.from(hash, 'hex')), decipher.final()]);
+        return decryptedData.toString();
 
-        return decrpyted.toString();
     }
 
     return hash;
